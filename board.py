295a61a88034ced92e38d84cb40109650f4c460f
Carlos Garcia <carlos.grh@outlook.com>
2022-09-26 07:58:06 -0500

    creating position change based on player response

--- board.py
+++ board.py
@@ -36,6 +36,18 @@ class Board():
         pass
 
 
-    def update_board(self, move_col, move_row):
-        pass
+    def update_board(self, move_col, move_row, piece):
+        list_of_moves = [(1,1), (1,2), (1,3), (2,1), (2,2), (2,3), (3,1), (3,2), (3,3)]
+        checker = (move_col, move_row)
+        if list_of_moves[0] == checker:
+            self.board[self.positions[0]-1] = piece
+        elif list_of_moves[0] == checker:
+            self.board[self.positions[0]+1] = piece
+        elif list_of_moves[0] == checker:
+            self.board[self.positions[0]+3] = piece
+        elif list_of_moves[1] == checker:
+            self.board[self.positions[1]-1] = piece
+        elif list_of_moves[1] == checker:
+            self.board[self.positions[1]+1] = piece
+
 
